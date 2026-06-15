cc57e8af21e4dd241974a3f568c338f50d061791
paras25358-svg <paras25358@iiitd.ac.in>
2026-06-15 20:44:11 +0530

    Add combat state logic based on health and distance

--- ML_Bots/combat_modified.py
+++ ML_Bots/combat_modified.py
@@ -1,1 +1,34 @@
+ATTACK_RANGE = 150
 
+CRITICAL_TARGET_HP = 25
+
+FINISH_OFF_HP = 10
+
+RETREAT_HP = 30
+
+
+# Testing Values
+
+bot_health = 25
+enemy_health = 5
+distance = 50
+
+
+# FSM
+
+if enemy_health <= FINISH_OFF_HP:
+    bot_state = "ATTACK"
+elif bot_health < RETREAT_HP:
+    bot_state = "RETREAT"
+elif distance <= ATTACK_RANGE:
+    bot_state = "ATTACK"
+else:
+    bot_state = "CHASE"
+
+print("Bot Health:", bot_health)
+print("Enemy Health:", enemy_health)
+print("Distance:", distance)
+
+print()
+
+print("Bot State:", bot_state)
